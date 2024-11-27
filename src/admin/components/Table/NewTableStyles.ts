112607311de8852f816183cfcfbacd76d7d4962a
Sukhrob <211782s@jdu.uz>
2024-11-27 16:11:47 +0500

    +websocket

---
+++ src/admin/components/Table/NewTableStyles.ts
@@ -0,0 +1,96 @@
+// styles.ts
+import { styled } from '@mui/material/styles';
+import {
+  TableCell,
+  Box,
+  Typography,
+  ButtonGroup,
+  Button,
+  Checkbox,
+} from '@mui/material';
+
+export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  height: '60px',
+  border: '1px solid rgba(224, 224, 224, 1)',
+  fontSize: '28px',
+}));
+
+export const EmployeeCell = styled('div')<{ status: boolean | null }>(({ status, theme }) => ({
+  backgroundColor: status === true ? '#fafafa' : status === false ? '#E53935' : 'transparent',
+  padding: theme.spacing(1),
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  fontSize: '28px',
+}));
+
+export const PaginationContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: theme.spacing(2),
+  marginBottom: theme.spacing(2),
+}));
+
+export const PageIndicator = styled(Typography)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  lineHeight: 1,
+  padding: theme.spacing(2),
+  color: '#105E82',
+}));
+
+export const StyledButtonGroup = styled(ButtonGroup)(() => ({
+  '& .MuiButton-outlined': {
+    borderColor: '#105E82',
+    color: '#105E82',
+    '&:hover': {
+      backgroundColor: 'rgba(16, 94, 130, 0.04)',
+      borderColor: '#105E82',
+    },
+    '&:disabled': {
+      borderColor: 'rgba(16, 94, 130, 0.3)',
+      color: 'rgba(16, 94, 130, 0.3)',
+    },
+  },
+  '& .MuiButton-outlined:not(:last-child)': {
+    borderRightColor: '#105E82',
+  },
+  '& .MuiButton-outlined:not(:first-child):not(:last-child)': {
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    '&:hover': {
+      borderLeftColor: 'transparent',
+      borderRightColor: 'transparent',
+    },
+  },
+}));
+
+export const StyledButton = styled(Button)(() => ({
+  '&.MuiButton-outlined': {
+    borderColor: '#105E82',
+    color: '#105E82',
+    '&:hover': {
+      borderColor: '#105E82',
+      backgroundColor: 'rgba(16, 94, 130, 0.04)',
+    },
+  },
+}));
+
+export const StyledCheckbox = styled(Checkbox)(() => ({
+  '&.MuiCheckbox-root': {
+    color: '#105E82',
+    '&.Mui-checked': {
+      color: '#105E82',
+    },
+    '&.Mui-indeterminate': {
+      color: '#105E82',
+    },
+  },
+}));
